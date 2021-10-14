26a328ea2b431cd12dd685c9e94880e75199fbae
Abelson Abueg <abuega18@students.ecu.edu>
2021-10-14 19:59:06 -0400

    Updated Files

--- Partial Solution Files/dir.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef __linux__
-bool is_directory(const std::string& filename);
-#endif
-
-std::vector<std::string> file_list(const std::string& dirname, std::vector<std::string>& files);
